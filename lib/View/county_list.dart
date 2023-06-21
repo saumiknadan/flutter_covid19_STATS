8e28b7bd92bc4e2a1053bd9e5a4b02aa63e3b4d6
Saumik Chowdhury <47342920+saumiknadan@users.noreply.github.com>
2023-06-22 00:51:29 +0600

    Search Box

--- lib/View/county_list.dart
+++ lib/View/county_list.dart
@@ -27,6 +27,9 @@ class _CountryListState extends State<CountryList> {
               padding: const EdgeInsets.all(8.0),
               child: TextFormField(
                 controller: searchController,
+                onChanged: (value) {
+                  setState(() {});
+                },
                 decoration: InputDecoration(
                     contentPadding: const EdgeInsets.symmetric(horizontal: 20),
                     hintText: 'Search with Country Name',
@@ -41,43 +44,80 @@ class _CountryListState extends State<CountryList> {
                 builder: (context, AsyncSnapshot<List<dynamic>> snapshot) {
                   if (!snapshot.hasData) {
                     return ListView.builder(
-                      itemCount: 4,
-                      itemBuilder: (context, index) {
-                      return Shimmer.fromColors(
-                        baseColor: Colors.grey.shade700,
-                        highlightColor: Colors.grey.shade100,
-                        child: Column(
-                          children: [
-                          ListTile(
-                            title: Container(height: 10, width: 89, color: Colors.white, ),
-                            subtitle: Container(height: 10, width: 89, color: Colors.white, ),
-                            
-                            leading: Container(height: 10, width: 89, color: Colors.white, ),
-                            )],
-                        ),
-                         );
-                    });
-                  
+                        itemCount: 4,
+                        itemBuilder: (context, index) {
+                          return Shimmer.fromColors(
+                            baseColor: Colors.grey.shade700,
+                            highlightColor: Colors.grey.shade100,
+                            child: Column(
+                              children: [
+                                ListTile(
+                                  title: Container(
+                                    height: 10,
+                                    width: 89,
+                                    color: Colors.white,
+                                  ),
+                                  subtitle: Container(
+                                    height: 10,
+                                    width: 89,
+                                    color: Colors.white,
+                                  ),
+                                  leading: Container(
+                                    height: 10,
+                                    width: 89,
+                                    color: Colors.white,
+                                  ),
+                                )
+                              ],
+                            ),
+                          );
+                        });
                   } else {
                     return ListView.builder(
-                      itemCount: snapshot.data!.length,
-                      itemBuilder: (context, index) {
-                      return Column(
-                        children: [
-                          ListTile(
-                            title: Text(snapshot.data![index]['country']),
-                            subtitle: Text(snapshot.data![index]['cases'].toString()),
-                            leading: Image(
-                              height:50,
-                              width: 50,
-                              image: NetworkImage(
-                                snapshot.data![index]['countryInfo']['flag']
-                              ),
-                            )
-                          )
-                        ],
-                      );
-                    });
+                        itemCount: snapshot.data!.length,
+                        itemBuilder: (context, index) {
+                          String name = snapshot.data![index]['country'];
+
+                          if (searchController.text.isEmpty) {
+                            return Column(
+                              children: [
+                                ListTile(
+                                    title:
+                                        Text(snapshot.data![index]['country']),
+                                    subtitle: Text(snapshot.data![index]
+                                            ['cases']
+                                        .toString()),
+                                    leading: Image(
+                                      height: 50,
+                                      width: 50,
+                                      image: NetworkImage(snapshot.data![index]
+                                          ['countryInfo']['flag']),
+                                    ))
+                              ],
+                            );
+                          } else if (name
+                              .toLowerCase()
+                              .contains(searchController.text.toLowerCase())) {
+                            return Column(
+                              children: [
+                                ListTile(
+                                    title:
+                                        Text(snapshot.data![index]['country']),
+                                    subtitle: Text(snapshot.data![index]
+                                            ['cases']
+                                        .toString()),
+                                    leading: Image(
+                                      height: 50,
+                                      width: 50,
+                                      image: NetworkImage(snapshot.data![index]
+                                          ['countryInfo']['flag']),
+                                    ))
+                              ],
+                            );
+                          } else {
+                            return Container();
+                          }
+                        });
                   }
                 },
               ),
